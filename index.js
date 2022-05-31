344f4247ff34877ede93c70adf5ac2e0be89f23d
freddex <fredjuiceman@yahoo.com>
2022-05-30 20:47:27 -0400

    commit tree be487b919fb32bc2008594fd0d9d3268e1558918

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 function addingEventListener() {
 }
+const main = document.getElementById('main');
+// const input = document.getElementById("button")
+// input.addingEventListener("click", =>
+//     alert("I was clicked")
+// )
+
+const input = document.querySelector("input")
+
+input.addEventListener("keydown", function(f) {
+    if (f.which === 100) {
+      console.log("default prevented")
+      return f.preventDefault()
+    } else {
+      console.log('Not a "g"')
+    }
+  })
